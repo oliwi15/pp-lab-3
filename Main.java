2389765651410d2285520fb4d15faf438989140e
Oliwia Kowalczyk <oliwia.szelag@op.pl>
2024-04-07 14:14:28 +0200

    chore: add new file

---
+++ Main.java
@@ -0,0 +1,12 @@
+
+import java.awt.Point;
+
+public class Main {
+    public static void main(String[] args) {
+        Point point = new Point(3, 5);
+
+        System.out.println("Współrzędne punktu:");
+        System.out.println("x: " + point.getX());
+        System.out.println("y: " + point.getY());
+    }
+}
