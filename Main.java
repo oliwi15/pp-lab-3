53ac40c455f8da3b3d421075f8ff2d8fae49dbc1
Oliwia Kowalczyk <oliwia.szelag@op.pl>
2024-04-07 15:43:10 +0200

    chore: add new file

--- Main.java
+++ Main.java
@@ -1,17 +1,20 @@
-
 import geometry.*;
 
 public class Main {
     public static void main(String[] args) {
+        circle[] circles = new circle[2];
+        circles[0] = new circle(new Point(0, 0), 5.0);
+        circles[1] = new ColoredCircle(new Point(2, 2), 3.0, "red");
 
-        Point center = new Point(3, 4);
+        for (circle circle : circles) {
+            double area = circle.GetArea();
+            System.out.println("Pole koła: " + area);
 
-        ColoredCircle coloredCircle = new ColoredCircle(center, 5.0, "blue");
+            if (circle instanceof ColoredCircle) {
+                String color = ((ColoredCircle) circle).getColor();
+                System.out.println("Kolor koła: " + color);
 
-        System.out.println(
-                "Środek koła: (" + coloredCircle.getCenter().getX() + ", " + coloredCircle.getCenter().getY() + ")");
-        System.out.println("Promień koła: " + coloredCircle.GetRadius());
-        System.out.println("Kolor koła: " + coloredCircle.getColor());
+            }
+        }
     }
-
 }
