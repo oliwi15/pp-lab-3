ee75236e967228e4fc49459547c803edd551a9ac
Oliwia Kowalczyk <oliwia.szelag@op.pl>
2024-04-07 16:00:37 +0200

    chore: add new file

--- Main.java
+++ Main.java
@@ -1,15 +1,15 @@
-import geometry.Rectangle;
+
+import geometry.Square;
 
 public class Main {
     public static void main(String[] args) {
-        Rectangle rectangle = new Rectangle(4.0, 6.0);
+        Square square = new Square(5.0);
 
-        double area = rectangle.calculateArea();
-        double perimeter = rectangle.calculatePerimeter();
+        double area = square.calculateArea();
+        double perimeter = square.calculatePerimeter();
 
-        System.out.println("Długość prostokąta: " + rectangle.getLength());
-        System.out.println("Szerokość prostokąta: " + rectangle.getWidth());
-        System.out.println("Pole prostokąta: " + area);
-        System.out.println("Obwód prostokąta: " + perimeter);
+        System.out.println("Długość boku kwadratu: " + square.getLength());
+        System.out.println("Pole kwadratu: " + area);
+        System.out.println("Obwód kwadratu: " + perimeter);
     }
 }
---
+++ geometry/Square.java
@@ -0,0 +1,11 @@
+package geometry;
+
+import geometry.Rectangle;
+
+public class Square {
+    public class Square extends Rectangle {
+        public Square(double sideLength) {
+            super(sideLength, sideLength);
+        }
+    }
+}
